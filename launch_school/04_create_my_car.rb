ef3cce41617801e200be7db62c88f1c68043bd94
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-11 00:17:32 +0000

    write code to demonstrate how self works within classes and objects;

--- launch_school/04_create_my_car.rb
+++ launch_school/04_create_my_car.rb
@@ -48,3 +48,14 @@ lumina.brake(20)
 lumina.current_speed
 lumina.shut_down
 lumina.current_speed
+
+class MyCar
+
+  # code omitted for brevity...
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+MyCar.gas_mileage(13, 351)  # => "27 miles per gallon of gas"
